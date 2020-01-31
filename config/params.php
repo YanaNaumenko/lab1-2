82f17a058cfa8f92d3643401510ec8e46174aec6
LizzardJALPOS <59031537+YanaNaumenko@users.noreply.github.com>
2020-01-31 14:58:40 +0300

    first commit

---
+++ config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'lolnopeyouidiote@gmail.com',
+    'senderEmail' => 'lolnopeyouidiote@gmail.com',
+    'senderName' => 'Example.com mailer',
+    'emailto' => 'lolnopeyouidiote@gmail.com',
+];
